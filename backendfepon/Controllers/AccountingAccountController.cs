61864c32fb11896bdf47f933991d5f09c617df14
JReveloZen <javier.revelo@epn.edu.ec>
2024-07-13 05:23:34 -0500

    feat, parsing account

--- backendfepon/Controllers/AccountingAccountController.cs
+++ backendfepon/Controllers/AccountingAccountController.cs
@@ -162,6 +162,11 @@ namespace backendfepon.Controllers
                 var createdAccountingDTO = new AccountingAccountDTO
                 {
                     id= accountingA.Account_Id,
+                    accountName = cy.DecryptStringFromBytes_Aes(accountingA.Account_Name, _key, _iv),
+                    accountType = accountingA.AccountType.Account_Type_Name,
+                    currentValue = Decimal.Parse( cy.DecryptStringFromBytes_Aes(accountingA.Current_Value, _key, _iv)),
+                    date = DateTime.Parse( cy.DecryptStringFromBytes_Aes(accountingA.Initial_Balance_Date, _key, _iv)).ToString("dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    initialBalance= Decimal.Parse(cy.DecryptStringFromBytes_Aes(accountingA.Initial_Balance_Date, _key, _iv))
                 };
                 return CreatedAtAction(nameof(GetAccountingAccount), new { id = createdAccountingDTO.id}, createdAccountingDTO);
             }
